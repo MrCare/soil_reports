e98d2c9e79eddf176d0912941a3782d8f77be8ba
MacCar <534192336car@gmail.com>
2024-06-27 11:57:51 +0800

    ✨ feat(增加表66):

--- d_suiti.py
+++ d_suiti.py
@@ -3,7 +3,7 @@ Author: Mr.Car
 Date: 2024-06-21 15:12:58
 '''
 import os
-from inner import QUAL_74, QUAL_72, SUITI_64, SUITI_67, SUITI_71, SUITI_167
+from inner import QUAL_74, QUAL_72, SUITI_64, SUITI_67, SUITI_71, SUITI_167, SUITI_66
 from alive_progress import alive_bar
 from inner.share import *
 from inner.load_config import ConfigLoader
@@ -92,6 +92,18 @@ def type_167(file_pth, xls_template_path=inner_xls_template_path, out_file_pth=N
         bar()
     return "Done!"
 
+def type_66(file_pth, xls_template_path=inner_xls_template_path, out_file_pth=None):
+    total_steps = 1 + 1
+    if not out_file_pth:
+        out_file_pth = os.path.join(os.path.dirname(file_pth), 'reports_result.xlsx')
+    with alive_bar(total_steps, title="66:适宜类面积:") as bar:
+        df = read_and_prepare_file(file_pth)
+        wb = get_wb(xls_template_path)
+        wb = SUITI_66.statistics_all(df, yaml_data, wb, bar)
+        save_xls(wb, out_file_pth)
+        bar()
+    return "Done!"
+
 def suiti(suti_pth, out_file_pth=None, xls_template_path=inner_xls_template_path):
     type_56(suti_pth, xls_template_path, out_file_pth)
     type_62(suti_pth, out_file_pth, out_file_pth)
@@ -99,7 +111,8 @@ def suiti(suti_pth, out_file_pth=None, xls_template_path=inner_xls_template_path
     type_67(suti_pth, out_file_pth, out_file_pth)
     type_71(suti_pth, out_file_pth, out_file_pth)
     type_167(suti_pth, out_file_pth, out_file_pth)
+    type_66(suti_pth, out_file_pth, out_file_pth)
 
 if __name__ == "__main__":
     # type_167("/Users/car/Work/2023workPlan/三普土壤制图工具/测试与新需求/0617/样例数据0617/适宜性评价.shp")
-    type_167("/Users/car/Project/soilCli/reports/test_data/suiti_result/suiti_result_short.shp")
+    type_66("/Users/car/Project/soilCli/reports/test_data/suiti_result/suiti_result_short.shp")
---
+++ inner/SUITI_66.py
@@ -0,0 +1,87 @@
+'''
+Author: Mr.Car
+Date: 2024-06-26 13:10:30
+'''
+import pandas as pd
+import numpy as np
+import re
+from .share import fill_template, fill_title, get_sheet, XlsPosUtil, _get_devided_result
+from .SUITI_71 import _create_target_df
+from .exception import logging
+
+xpu = XlsPosUtil()
+
+def add_dl(row, field, new_field_name, new_field_name_rule):
+    '''
+    增加大类字段
+    '''
+    for i, each in enumerate(new_field_name_rule):
+        if re.match(each[1:-1], row[field]):
+            return new_field_name[i]
+
+def _get_result_list(pivot_df_first, pivot_df_second, columns_values, col_name_list):
+    result_list = []
+    percent_list = []
+    for each_col_name in col_name_list:
+        result = []
+        percent = []
+        if each_col_name[0] == 0:
+            for each_value in columns_values:
+                try:
+                    area = pivot_df_first.loc[each_col_name[1], each_value]
+                    result.append(area)
+                    percent.append(f"{(area / pivot_df_first.loc[each_col_name[1], 'All'] * 100):.2f}%")
+                except Exception as e:
+                    result.append(0)
+                    percent.append(f"{0 * 100:.2f}%")
+                    logging.error(f"SUITI_66: {e}", exc_info=True)
+        elif each_col_name[0] == 1:
+            for each_value in columns_values:
+                try:
+                    index_field = (each_col_name[1], each_col_name[2])
+                    area = pivot_df_second.loc[index_field, each_value]
+                    result.append(area)
+                    percent.append(f"{(area / pivot_df_second.loc[index_field, 'All'] * 100):.2f}%")
+                except Exception as e:
+                    result.append(0)
+                    percent.append(f"{0 * 100:.2f}%")
+                    logging.error(f"SUITI_66: {e}", exc_info=True)
+        result_list.append(result)
+        percent_list.append(percent)
+        
+    return np.transpose(result_list), np.transpose(percent_list)
+
+
+def statistics_all(df, yaml_data, wb, bar):
+    sheet_name = "SUITI_66"
+    title = yaml_data[sheet_name]["title"]
+    values = yaml_data[sheet_name]["values"]
+    field = yaml_data[sheet_name]["field"]
+    new_field = yaml_data[sheet_name]["new_field"]
+    new_field_name = yaml_data[sheet_name]["new_field_name"]
+    new_field_name_rule = yaml_data[sheet_name]['new_field_name_rule']
+    index = yaml_data[sheet_name]['index']
+    columns = yaml_data[sheet_name]['columns']
+    columns_values = yaml_data[sheet_name]['columns_values']
+    col_name_list = yaml_data[sheet_name]['col_name_list']
+    result_start_position = yaml_data[sheet_name]['result_start_position']
+    percent_start_position = yaml_data[sheet_name]['percent_start_position']
+
+    df[new_field] = df.apply(lambda row : add_dl(row, field, new_field_name, new_field_name_rule), axis=1)
+    sheet = get_sheet(wb, sheet_name)
+    pivot_df_first = pd.pivot_table(df, values=values, index=index[0], columns=columns, aggfunc="sum", margins=True)
+    pivot_df_first = _create_target_df(pivot_df_first, columns_values)
+    pivot_df_second = pd.pivot_table(df, values=values, index=index, columns=columns, aggfunc="sum", margins=True)
+    pivot_df_second = _create_target_df(pivot_df_second, columns_values)
+    result_list, percent_list = _get_result_list(pivot_df_first, pivot_df_second, columns_values, col_name_list)
+
+    sheet = get_sheet(wb, sheet_name)
+    fill_title(sheet, title)
+    for i, each_result in enumerate(result_list):
+        position = xpu.position_add_col(result_start_position, i)
+        fill_template(sheet, position, each_result, False, 1)
+    for i, each_percent in enumerate(percent_list):
+        position = xpu.position_add_col(percent_start_position, i)
+        fill_template(sheet, position, each_percent, False, 1)
+    bar()
+    return wb
